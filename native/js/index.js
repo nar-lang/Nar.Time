4027d3cf78c2e6bf47a958c37d5e773362fb3ea6
Sergey Dvoynikov <s@dvoyni.com>
2023-12-24 14:33:37 +0100

    Time.every implemented

--- native/js/index.js
+++ native/js/index.js
@@ -1,18 +1,32 @@
 export default function (runtime) {
+    function toPosix(time) {
+        return runtime.optionShallow("Oak.Time.Posix", "Millis", [runtime.float(time / 1000.0)]);
+    }
+
     runtime.afterRegistered(["Oak.Program"], () => {
         runtime.register("Oak.Time", {
             "after": (_delay) => {
                 return runtime.scope("Oak.Program").newTask((success, _) => {
                     setTimeout(() => {
-                        success(runtime.float(Date.now() / 1000.0));
+                        success(toPosix(Date.now()));
                     }, runtime.unwrap(_delay).$values[0]);
                 });
             },
             "atEndOfFrame": runtime.scope("Oak.Program").newTask((success, _) => {
                 requestAnimationFrame((time) => {
-                    success(runtime.float(time / 1000.0));
+                    success(toPosix(time));
                 });
             }),
+            "every": (_delay, _toMsg) => {
+                return runtime.scope("Oak.Program").newSub("Oak.Time.every", [_delay], _toMsg, (args, post) => {
+                    const interval = setInterval(() => {
+                        post(toPosix(Date.now()));
+                    }, runtime.unwrap(_delay).$values[0]);
+                    return () => {
+                        clearInterval(interval);
+                    }
+                });
+            },
         }, ["Oak.Program"]);
     });
 }
